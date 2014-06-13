a0d983839323cbce162d92fc63f80bfae5cf0b2a
AIR Admin <air-admin@air.(none)>
2014-06-12 23:37:02 -0400

    fix names

--- touch_tomorrow_demo/src/object_state_publisher.py
+++ touch_tomorrow_demo/src/object_state_publisher.py
@@ -43,11 +43,11 @@ class ObjectStatePublisher:
         
         msg1.color.a = 1.0
         msg1.color.r = 0.8
-        msg1.color.g = 0.7
-        msg1.color.b = 0.3
-        msg1.mesh_resource = "package://touch_tomorrow_demo/models/dragon_head.dae"
+        msg1.color.g = 0.1
+        msg1.color.b = 0.5
+        msg1.mesh_resource = "package://touch_tomorrow_demo/models/elephant.dae" 
       
-        scale_factor_1 = 1.5
+        scale_factor_1 = 0.8
 
         msg2 = Marker()
         msg2.header.frame_id = "world"
@@ -59,11 +59,11 @@ class ObjectStatePublisher:
         
         msg2.color.a = 1.0
         msg2.color.r = 0.9
-        msg2.color.g = 0.9
-        msg2.color.b = 0.9
-        msg2.mesh_resource = "package://touch_tomorrow_demo/models/elephant.dae"
+        msg2.color.g = 0.01
+        msg2.color.b = 0.05
+        msg2.mesh_resource = "package://touch_tomorrow_demo/models/chevy.dae"
 
-        scale_factor_2 = 0.8
+        scale_factor_2 = 1.0
 
         msg3 = Marker()
         msg3.header.frame_id = "world"
@@ -77,9 +77,9 @@ class ObjectStatePublisher:
         msg3.color.r = 0.8
         msg3.color.g = 0.7
         msg3.color.b = 0.3
-        msg3.mesh_resource = "package://touch_tomorrow_demo/models/chevy.dae"
+        msg3.mesh_resource = "package://touch_tomorrow_demo/models/dragon_head.dae" 
 
-        scale_factor_3 = 1.0
+        scale_factor_3 = 1.5
 
         object_1_in_scene = 0
         object_2_in_scene = 0
@@ -89,7 +89,7 @@ class ObjectStatePublisher:
 
         while not rospy.is_shutdown():
 
-            try:
+            try: # ELEPHANT
                 now = rospy.Time.now()
                 p1, q1 = listener.lookupTransform( '/world', "/vicon/TouchTomorrow1/TouchTomorrow1" , rospy.Time(0))
             except :
@@ -98,30 +98,28 @@ class ObjectStatePublisher:
                 if object_1_in_scene <= min_seen : object_1_in_scene += 1
             #print "got transform3" 
 
-            try:
+            try: # CAMARO
                 now = rospy.Time.now()
-                p2, q2 = listener.lookupTransform('/world', "/vicon/TouchTomorrow2/TouchTomorrow2", rospy.Time(0))
-            except:
+                p2, q2 = listener.lookupTransform( '/world', "/vicon/TouchTomorrow2/TouchTomorrow2" , rospy.Time(0))
+            except: # (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 object_2_in_scene = 0
             else:
                 if object_2_in_scene <= min_seen : object_2_in_scene += 1
 
-            try:
+            try: # DRAGON
                 now = rospy.Time.now()
-                #listener.waitForTransform("/vicon/TouchTomorrow1/TouchTomorrow1", '/world', now, rospy.Duration(2.0))
-                p3, q3 = listener.lookupTransform( '/world', "/vicon/TouchTomorrow3/TouchTomorrow3" , rospy.Time(0))
-            except: # (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                p3, q3 = listener.lookupTransform('/world', "/vicon/TouchTomorrow3/TouchTomorrow3", rospy.Time(0))
+            except:
                 object_3_in_scene = 0
             else:
                 if object_3_in_scene <= min_seen : object_3_in_scene += 1
-                
 
             print "objects ", object_1_in_scene, object_2_in_scene, object_3_in_scene
 
             #print p1
             # print q1
 
-            if object_1_in_scene > min_seen :
+            if object_1_in_scene > min_seen : # ELEPHANT
 
                 msg1.pose.position.x =  p1[0] #- 0.3 #+ offset_x -0.3 +
                 msg1.pose.position.y =  p1[1] #- 11.3  #+ offset_y -11.3 +
@@ -136,22 +134,18 @@ class ObjectStatePublisher:
                 msg1.scale.x = 1. * scale_factor_1
                 msg1.scale.y = 1. * scale_factor_1
                 msg1.scale.z = 1. * scale_factor_1
-                
+
                 # Offset in world
                 pose = Pose()
-                pose.position.x =  -0.3 #+ offset_x -0.3 +
-                pose.position.y =  -11.3  #+ offset_y -11.3 +
-                pose.position.z =  -3.7 #+ offset_z -3.7 +
+                pose.position.x =  -3.8 #+ offset_x -0.3 +
+                pose.position.y =  -2.  #+ offset_y -11.3 +
+                pose.position.z =  -1.5 #+ offset_z -3.7 +
                 msg1.pose = ComposePoses( msg1.pose, pose )
 
-                # Offset in object frame
-                # mat = MakeTransformMatrix(rodrigues([pi/2, 0, 0]), transpose(matrix([0, 0, 0])))
-                # msg1.pose = ComposePoses( PoseFromMatrix( mat ), msg1.pose )
+                # self.object_pub.publish( msg1 )
 
-                self.object_pub.publish( msg1 )
+            if object_2_in_scene > min_seen : # CAMARO
 
-            if object_2_in_scene > min_seen :
-            
                 msg2.pose.position.x =  + p2[0] #+ offset_x
                 msg2.pose.position.y =  + p2[1] #+ offset_y
                 msg2.pose.position.z =  + p2[2] #+ offset_z
@@ -161,22 +155,19 @@ class ObjectStatePublisher:
                 msg2.pose.orientation.w = q2[3]
                 msg2.scale.x = 1. * scale_factor_2
                 msg2.scale.y = 1. * scale_factor_2
-                msg2.scale.z = 1. * scale_factor_2
+                msg2.scale.z = 1. * scale_factor_3
 
-                # Offset in world
                 pose = Pose()
-                pose.position.x =  -3.8 #+ offset_x -0.3 +
-                pose.position.y =  -2.  #+ offset_y -11.3 +
-                pose.position.z =  -1.5 #+ offset_z -3.7 +
+                pose.position.x =  -1.8 #+ offset_x -0.3 +
+                pose.position.y =  -1.9  #+ offset_y -11.3 +
+                pose.position.z =  0.0 #+ offset_z -3.7 +
                 msg2.pose = ComposePoses( msg2.pose, pose )
-
                 # Offset in object frame
                 # mat = MakeTransformMatrix(rodrigues([pi/2, 0, 0]), transpose(matrix([0, 0, 0])))
                 # msg1.pose = ComposePoses( PoseFromMatrix( mat ), msg1.pose )
+                # self.object_pub.publish( msg2 )
 
-                self.object_pub.publish( msg2 )
-
-            if object_3_in_scene > min_seen :
+            if object_3_in_scene > min_seen : # DRAGON
 
                 msg3.pose.position.x =  + p3[0] #+ offset_x
                 msg3.pose.position.y =  + p3[1] #+ offset_y
@@ -189,13 +180,19 @@ class ObjectStatePublisher:
                 msg3.scale.y = 1. * scale_factor_3
                 msg3.scale.z = 1. * scale_factor_3
 
+                # Offset in world
                 pose = Pose()
-                pose.position.x =  -1.8 #+ offset_x -0.3 +
-                pose.position.y =  -1.9  #+ offset_y -11.3 +
-                pose.position.z =  0.0 #+ offset_z -3.7 +
+                pose.position.x =  -0.3 #+ offset_x -0.3 +
+                pose.position.y =  -11.3  #+ offset_y -11.3 +
+                pose.position.z =  -3.7 #+ offset_z -3.7 +
                 msg3.pose = ComposePoses( msg3.pose, pose )
 
+                # Offset in object frame
+                # mat = MakeTransformMatrix(rodrigues([pi/2, 0, 0]), transpose(matrix([0, 0, 0])))
+                # msg1.pose = ComposePoses( PoseFromMatrix( mat ), msg1.pose )
+
                 self.object_pub.publish( msg3 )
+
             
     def loop(self, hz=1.):
         r = rospy.Rate(hz) 
